1b62143ae6590bdd995e387fbe0b969640294bf9
Aniket <aniketraj28042003@gmail.com>
2025-05-06 19:03:35 +0530

    add: interface: added interfaces for notification and queue data

---
+++ src/interface/notification.interface.ts
@@ -0,0 +1,15 @@
+export interface INotification {
+  user_email: string;
+  seats: number;
+  total_price: number;
+  flight_number: string;
+  departure_time: Date;
+  arrival_time: Date;
+  airplane_name: string;
+  departure_airport_name: string;
+  departure_airport_city: string;
+  departure_airport_country: string;
+  arrival_airport_name: string;
+  arrival_airport_city: string;
+  arrival_airport_country: string;
+}
